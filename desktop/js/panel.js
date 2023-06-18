e43fb0feb5b84240ab892bf5316d7ed35a77e003
limad <l.imad44@gmail.com>
2023-06-18 13:33:06 +0200

    18

--- desktop/js/panel.js
+++ desktop/js/panel.js
@@ -1,132 +1,341 @@
-/* This file is part of Jeedom.
-*
-* Jeedom is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* Jeedom is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
-*/
 
-  $('.displayEnergyDetail').off('click').on('click',function(){
-    $('.energyDetail[data-id='+$(this).attr('data-id')+']').toggle();
-  });
-  
-  $('.bt_changePeriod').on('click',function(){
-    var url = document.URL
-    var newAdditionalURL = '';
-    var tempArray = url.split("?");
-    var baseURL = tempArray[0];
-    var aditionalURL = tempArray[1];
-    var temp = '';
-    if(aditionalURL)  {
-      var tempArray = aditionalURL.split('&');
-      for ( var i in tempArray ){
-        if(tempArray[i].indexOf('period') == -1){
-          newAdditionalURL += temp+tempArray[i];
-          temp = "&";
-        }
-      }
-    }
-    jeedom.history.chart = [];
-    var url = baseURL+'?'+newAdditionalURL+temp+'period='+$(this).attr('data-period')
-    loadPage(url.replace('#', ''));
-  });
-  
-  var graphOption = {
-    showNavigator : false,
-    showLegend : false,
-    showScrollbar : false,
-    showTimeSelector : false,
-    option : {displayAlert:false}
-  }
-  function initGraph(){
-    jeedom.history.chart = [];
-    var colors = Highcharts.getOptions().colors
-    for(var i in data.graphData){
-      if (isNaN(i)) {
-        continue;
-      }
-      nbSerie = 0;
-      graphOption.el = 'div_chartDay'+i;
-      graphOption.height = $('#'+graphOption.el).closest('.row').find('table').height();
-      graphOption.dateStart = data.graphData.day.start;
-      graphOption.dateEnd = data.graphData.day.end;
-      graphOption.option = {graphColor:colors[nbSerie]}
-      if(isset(data.graphData[i].cmd_day_consumption)){
-        graphOption.cmd_id = data.graphData[i].cmd_day_consumption;
-        jeedom.history.drawChart(JSON.parse(JSON.stringify(graphOption)));
-        nbSerie++
-      }
-      graphOption.option = {graphColor:colors[nbSerie]}
-      if(isset(data.graphData[i].cmd_day_cost)){
-        graphOption.cmd_id = data.graphData[i].cmd_day_cost;
-        jeedom.history.drawChart(JSON.parse(JSON.stringify(graphOption)));
-        nbSerie++
-      }
-      graphOption.option = {graphColor:colors[nbSerie]}
-      if(isset(data.graphData[i].cmd_instant)){
-        graphOption.cmd_id = data.graphData[i].cmd_instant;
-        jeedom.history.drawChart(JSON.parse(JSON.stringify(graphOption)));
-        nbSerie++
-      }
-      if(isset(data.graphData[i].cmd_performance)){
-        graphOption.el = 'div_chartPerformanceDay'+i;
-        graphOption.cmd_id = data.graphData[i].cmd_performance.id;
-        if(isset(data.graphData[i].cmd_performance.startDate)){
-          graphOption.dateStart =data.graphData[i].cmd_performance.startDate;
-        }
-        if(isset(data.graphData[i].cmd_performance.endDate)){
-          graphOption.dateEnd = data.graphData[i].cmd_performance.endDate;
-        }
-        jeedom.history.drawChart(JSON.parse(JSON.stringify(graphOption)));
-      }
-    }
-    
-    new Highcharts.Chart({
-      chart: {
-        zoomType: 'x',
-        renderTo: 'graph_division',
-        alignTicks: false,
-        spacingBottom: 5,
-        spacingTop: 5,
-        spacingRight: 5,
-        spacingLeft: 5,
-        type: 'pie',
-        backgroundColor:'transparent'
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+if((!isset(userProfils.doNotAutoHideMenu) || userProfils.doNotAutoHideMenu != 1) && !jQuery.support.touch){
+  $('#sd_objectList').hide();
+  $('#div_graphiqueDisplay').removeClass('col-xs-10').addClass('col-xs-12');
+
+  $('#bt_displayObjectList').on('mouseenter',function(){
+     var timer = setTimeout(function(){
+      $('#bt_displayObjectList').find('i').hide();
+      $('#div_graphiqueDisplay').addClass('col-xs-10').removeClass('col-xs-12');
+      $('#sd_objectList').show();
+      $(window).resize();
+  }, 100);
+     $(this).data('timerMouseleave', timer)
+ }).on("mouseleave", function(){
+    clearTimeout($(this).data('timerMouseleave'));
+});
+
+ $('#sd_objectList').on('mouseleave',function(){
+   var timer = setTimeout(function(){
+     $('#sd_objectList').hide();
+     $('#bt_displayObjectList').find('i').show();
+     $('#div_graphiqueDisplay').removeClass('col-xs-10').addClass('col-xs-12');
+     setTimeout(function(){
+      $(window).resize();
+     },100);
+     setTimeout(function(){
+      $(window).resize();
+     },300);
+      setTimeout(function(){
+      $(window).resize();
+     },500);
+ }, 300);
+   $(this).data('timerMouseleave', timer);
+}).on("mouseenter", function(){
+clearTimeout($(this).data('timerMouseleave'));
+});
+}
+
+
+
+$(".in_datepicker").datepicker();
+
+$('#bt_validChangeDate').on('click', function () {
+  getDatas(object_id, $('#in_startDate').value(), $('#in_endDate').value());
+});
+
+$('#cb_powerLissage').on('change',function(){
+  if($(this).value() == 1){
+      window.location.href=$(this).attr('data-href')+'&powerDisplay=1h';
+  }else{
+     window.location.href=$(this).attr('data-href')+'&powerDisplay=15min';   
+ }
+});
+
+setTimeout(function(){ getDatas(object_id); }, 1);
+
+function getDatas(_object_id, _dateStart, _dateEnd) {
+  $.ajax({
+      type: 'POST',
+      url: 'plugins/energy/core/ajax/energy.ajax.php',
+      data: {
+          action: 'getData',
+          object_id: init(_object_id),
+          dateStart: init(_dateStart),
+          dateEnd: init(_dateEnd),
+          energyType : energyType
       },
-      title: {text: ''},
-      credits: {text: '', href: '' },
-      exporting: {enabled: $.mobile},
-      tooltip: {
-        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
-        valueDecimals: 2,
+      dataType: 'json',
+      error: function (request, status, error) {
+          handleAjaxError(request, status, error);
       },
-      plotOptions: {
-        series: {
-          animation: {
-            duration : (getUrlVars('report') == 1) ? 0 : 1000
+      success: function (data) {
+          if (data.state != 'ok') {
+              $('#div_alert').showAlert({message: data.result, level: 'danger'});
+              return;
           }
-        },
-        pie: {
-          allowPointSelect: true,
-          cursor: 'pointer',
-          dataLabels: {
-            enabled: true,
-            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-            style: {color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'}
+          $('.objectName').empty().append(data.result.object.name);
+          $('#div_energy').setValues(data.result.datas, '.energyAttr');
+          $('#div_energy').setValues(data.result, '.energyAttr');
+
+          var series = []
+          series.push({
+              step: true,
+              name: '{{Coût}} ('+data.result.currency+')',
+              data:  data.result.datas.cost,
+              type: 'column',
+              stack : 1,
+              unite : data.result.currency,
+              stacking : 'normal',
+              dataGrouping: {
+                  approximation: "sum",
+                  enabled: true,
+                  forced: true,
+                  units: [[groupBy,[1]]]
+              },
+              tooltip: {
+                  valueDecimals: 2
+              },
+          });
+          drawSimpleGraph('div_graphCost',series);
+          
+          
+           if(energyType != 'electricity'){
+              var series = []
+              if(isset(data.result.datas.totalConsumption)){
+                 series.push({
+                  name: '{{Consommation total}} ('+data.result.datas.consumptionUnite+')',
+                  data:  data.result.datas.totalConsumption,
+                  type: 'column',
+                  stack : 1,
+                  dataGrouping: {
+                      approximation: "sum",
+                      enabled: true,
+                      forced: true,
+                      units: [[groupBy,[1]]]
+                  },
+                  tooltip: {
+                      valueDecimals: 2
+                  },
+              });
+             }
+             drawSimpleGraph('div_graphDetailConsumption',series);
+             return;
+         }
+
+         var series = []
+         if($('#cb_powerLissage').value() == 1){
+          var powerGroup = [['hour',[1]]]
+      }else{
+          var powerGroup = [['minute',[15]]]
+      }
+      for(var i in data.result.datas.power){
+          if($.isArray(data.result.datas.power[i]) && data.result.datas.power[i].length > 0){
+              series.push({
+                  step: true,
+                  name: i+' (W)',
+                  data:  data.result.datas.power[i],
+                  unite : 'W',
+                  type: 'area',
+                  stack : 1,
+                  stacking : 'normal',
+                  dataGrouping: {
+                      approximation: "high",
+                      enabled: true,
+                      forced: true,
+                      units: powerGroup
+                  },
+                  tooltip: {
+                      valueDecimals: 2
+                  },
+              });
+          }
+      }
+      drawSimpleGraph('div_graphPower',series);
+
+      var series = []
+      if(isset( data.result.datas.totalConsumption)){
+         series.push({
+          step: true,
+          name: '{{Consommation totale}} (kWh)',
+          data:  data.result.datas.totalConsumption,
+          type: 'areaspline',
+          stack : 2,
+          dataGrouping: {
+              approximation: "sum",
+              enabled: true,
+              forced: true,
+              units: [[groupBy,[1]]]
           },
-          showInLegend: false
-        }
-      },
-      series: [{name: 'pointer',data: data.division}]
-    });
+          stacking : 'normal',
+          tooltip: {
+              valueDecimals: 2
+          },
+      });
+     }
+     for(var i in data.result.datas.category){
+      if($.isArray(data.result.datas.category[i]) && data.result.datas.category[i].length > 0){
+          series.push({
+              step: true,
+              name: data.result.datas.translation[i]+' (kWh)',
+              data:  data.result.datas.category[i],
+              type: 'column',
+              stack : 1,
+              stacking : 'normal',
+              dataGrouping: {
+                  approximation: "sum",
+                  enabled: true,
+                  forced: true,
+                  units: [[groupBy,[1]]]
+              },
+              tooltip: {
+                  valueDecimals: 2
+              },
+          });
+      }
   }
-  
-  initGraph();
+  drawSimpleGraph('div_graphDetailConsumptionByCategorie',series);
+
+
+  var series = []
+  if(isset(data.result.datas.totalConsumption)){
+     series.push({
+      step: true,
+      name: '{{Consommation totale}} (kWh)',
+      data:  data.result.datas.totalConsumption,
+      type: 'areaspline',
+      stack : 2,
+      stacking : 'normal',
+      dataGrouping: {
+          approximation: "sum",
+          enabled: true,
+          forced: true,
+          units: [[groupBy,[1]]]
+      },
+      tooltip: {
+          valueDecimals: 2
+      },
+  });
+ }
+ for(var i in data.result.datas.object){
+  if($.isArray(data.result.datas.object[i]) && data.result.datas.object[i].length > 0){
+      series.push({
+          step: true,
+          name: i+' (kWh)',
+          data:  data.result.datas.object[i],
+          type: 'column',
+          stack : 1,
+          stacking : 'normal',
+          dataGrouping: {
+              approximation: "sum",
+              enabled: true,
+              forced: true,
+              units: [[groupBy,[1]]]
+          },
+          tooltip: {
+              valueDecimals: 2
+          },
+      });
+  }
+}
+drawSimpleGraph('div_graphDetailConsumptionByObject',series);
+
+}
+});
+}
+
+function drawSimpleGraph(_el, _serie) {
+  var legend = {
+      enabled: true,
+      borderColor: 'black',
+      borderWidth: 2,
+      shadow: true
+  };
+
+  new Highcharts.StockChart({
+      chart: {
+          zoomType: 'x',
+          renderTo: _el,
+          height: 350,
+          spacingTop: 0,
+          spacingLeft: 0,
+          spacingRight: 0,
+          spacingBottom: 0
+      },
+      credits: {
+          text: 'Copyright Jeedom',
+          href: 'https://www.jeedom.com',
+      },
+      navigator: {
+          enabled: false
+      },
+      rangeSelector: {
+          buttons: [{
+              type: 'week',
+              count: 1,
+              text: 'S'
+          }, {
+              type: 'month',
+              count: 1,
+              text: 'M'
+          }, {
+              type: 'year',
+              count: 1,
+              text: 'A'
+          }, {
+              type: 'all',
+              count: 1,
+              text: 'Tous'
+          }],
+          selected: 6,
+          inputEnabled: false
+      },
+      legend: legend,
+      tooltip: {
+          pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
+          valueDecimals: 2,
+      },
+      yAxis: {
+          format: '{value}',
+          showEmpty: false,
+          showLastLabel: true,
+          min: 0,
+          labels: {
+              align: 'right',
+              x: -5
+          }
+      },
+      xAxis: {
+          type: 'datetime'
+      },
+      scrollbar: {
+          barBackgroundColor: 'gray',
+          barBorderRadius: 7,
+          barBorderWidth: 0,
+          buttonBackgroundColor: 'gray',
+          buttonBorderWidth: 0,
+          buttonBorderRadius: 7,
+          trackBackgroundColor: 'none', trackBorderWidth: 1,
+          trackBorderRadius: 8,
+          trackBorderColor: '#CCC'
+      },
+      series: _serie
+  });
+}
+
--- desktop/php/panel.php
+++ desktop/php/panel.php
@@ -3,64 +3,151 @@ if (!isConnect()) {
 	throw new Exception('{{401 - Accès non autorisé}}');
 }
 if (init('object_id') == '') {
-	$object = jeeObject::byId($_SESSION['user']->getOptions('defaultDashboardObject'));
-} else {
-	$object = jeeObject::byId(init('object_id'));
+	$_GET['object_id'] = $_SESSION['user']->getOptions('defaultDashboardObject');
 }
+$object = jeeObject::byId(init('object_id'));
 if (!is_object($object)) {
 	$object = jeeObject::rootObject();
 }
+if (!is_object($object)) {
+	throw new Exception('{{Aucun objet racine trouvé}}');
+}
+
+sendVarToJs('object_id', $object->getId());
+sendVarToJs('energyType', init('energyType', 'electricity'));
+sendVarToJs('groupBy', init('groupBy', 'day'));
+if (init('groupBy', 'day') == 'day') {
+	$date = array(
+		'start' => init('startDate', date('Y-m-d', strtotime('-31 days ' . date('Y-m-d')))),
+		'end' => init('endDate', date('Y-m-d')),
+	);
+}
+if (init('groupBy', 'day') == 'month') {
+	$date = array(
+		'start' => init('startDate', date('Y-m-d', strtotime('-1 year ' . date('Y-m-d')))),
+		'end' => init('endDate', date('Y-m-d', strtotime('+1 days' . date('Y-m-d')))),
+	);
+}
+?>
+<div style="position : fixed;height:100%;width:15px;top:50px;left:0px;z-index:998;background-color:#f6f6f6;" id="bt_displayObjectList"><i class="fa fa-arrow-circle-o-right" style="color : #b6b6b6;"></i></div>
+
+
+<div class="row row-overflow" id="div_energy">
+  <div class="col-xs-2" id="sd_objectList" style="z-index:999">
+    <div class="bs-sidebar">
+      <ul id="ul_object" class="nav nav-list bs-sidenav">
+        <li class="nav-header">{{Liste objets}}</li>
+        <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
+        <?php
 $allObject = jeeObject::buildTree();
-if (count($object->getEqLogic(true, false, 'pulseCounter')) == 0) {
-	foreach ($allObject as $object_sel) {
-		if (count($object_sel->getEqLogic(true, false, 'pulseCounter')) > 0) {
-			$object = $object_sel;
-			break;
+foreach ($allObject as $object_li) {
+	if ($object_li->getIsVisible() == 1) {
+		$margin = 15 * $object_li->parentNumber();
+		if ($object_li->getId() == $object->getId()) {
+			echo '<li class="cursor li_object active" ><a href="index.php?v=d&m=energy&p=panel&object_id=' . $object_li->getId() . '&energyType=' . init('energyType', 'electricity') . '" style="position:relative;left:' . $margin . 'px;">' . $object_li->getHumanName(true) . '</a></li>';
+		} else {
+			echo '<li class="cursor li_object" ><a href="index.php?v=d&m=energy&p=panel&object_id=' . $object_li->getId() . '&energyType=' . init('energyType', 'electricity') . '" style="position:relative;left:' . $margin . 'px;">' . $object_li->getHumanName(true) . '</a></li>';
 		}
 	}
 }
-if (is_object($object)) {
-	$_GET['object_id'] = $object->getId();
-}
-sendVarToJs('object_id', init('object_id'));
-
-$pulseCounters = $object->getEqLogic(true, false, 'pulseCounter');
-$graphdata = array();
-sendVarToJs('jeedomBackgroundImg', 'plugins/pulseCounter/core/img/panel.jpg');
-$graphData['day'] = array('start' => date('Y-m-d', strtotime('now -3 month')), 'end' => date('Y-m-d', strtotime('now')));
 ?>
-<div class="row row-overflow" id="div_pulseCounter">
-	<div class="col-lg-2 reportModeHidden">
-		<div class="bs-sidebar">
-			<ul id="ul_object" class="nav nav-list bs-sidenav">
-				<li class="nav-header">{{Liste objets}}</li>
-				<li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
-				<?php
-				foreach ($allObject as $object_li) {
-					if ($object_li->getIsVisible() != 1 || count($object_li->getEqLogic(true, false, 'pulseCounter', null, true)) == 0) {
-						continue;
-					}
-					$margin = 5 * $object_li->parentNumber();
-					if ($object_li->getId() == init('object_id')) {
-						echo '<li class="cursor li_object active" ><a data-object_id="' . $object_li->getId() . '" href="index.php?v=d&p=panel&m=pulseCounter&object_id=' . $object_li->getId() . '" style="padding: 2px 0px;"><span style="position:relative;left:' . $margin . 'px;">' . $object_li->getHumanName(true) . '</span></a></li>';
-					} else {
-						echo '<li class="cursor li_object" ><a data-object_id="' . $object_li->getId() . '" href="index.php?v=d&p=panel&m=pulseCounter&object_id=' . $object_li->getId() . '" style="padding: 2px 0px;"><span style="position:relative;left:' . $margin . 'px;">' . $object_li->getHumanName(true) . '</span></a></li>';
-					}
-				}
-				?>
-			</ul>
-		</div>
-	</div>
-	<?php
-	if (init('report') != 1) {
-		echo '<div class="col-lg-10">';
-	} else {
-		echo '<div class="col-lg-12">';
-	}
-	$panel = pulseCounter::generatePanel('dashboard', init('period', config::byKey('savePeriod', 'pulseCounter')));
-	echo $panel['html'];
+   </ul>
+ </div>
+</div>
+
+<div class="col-xs-10" id="div_graphiqueDisplay">
+ <legend style="height: 40px;">
+  <i class="fa fa-picture-o"></i>  <span class="objectName"></span> {{du}}
+  <input class="form-control input-sm in_datepicker" id='in_startDate' style="display : inline-block; width: 150px;" value='<?php echo $date['start']?>'/> {{au}}
+  <input class="form-control input-sm in_datepicker" id='in_endDate' style="display : inline-block; width: 150px;" value='<?php echo $date['end']?>'/>
+  <a class="btn btn-success btn-sm tooltips" id='bt_validChangeDate' title="{{Attention une trop grande plage de dates peut mettre très longtemps à être calculée ou même ne pas s'afficher}}">{{Ok}}</a>
+  <center style="display:inline-block;">
+    <?php if (init('energyType', 'electricity') == 'electricity') {?>
+    <span class='label label-success' style="font-size: 0.9em;"><span class="energyAttr" data-l1key="total" data-l2key="power"></span> W</span>
+    <span class='label label-primary' style="font-size: 0.9em;"><span class="energyAttr" data-l1key="total" data-l2key="consumption"></span> <span class="energyAttr" data-l1key="consumptionUnite"></span></span>
+    <span class='label label-default' style="font-size: 0.9em;"><span class="energyAttr" data-l1key="total" data-l2key="cost"> </span> <span class="energyAttr" data-l1key="currency"></span>
+    <?php } else {
 	?>
+     <span class='label label-primary' style="font-size: 0.9em;"><span class="energyAttr" data-l1key="total" data-l2key="consumption"></span> <span class="energyAttr" data-l1key="consumptionUnite"></span></span>
+     <span class='label label-default' style="font-size: 0.9em;"><span class="energyAttr" data-l1key="total" data-l2key="cost"> </span> <span class="energyAttr" data-l1key="currency"></span>
+     <?php }
+?>
+
+   </center>
+
+
+   <span class="pull-right">
+    <?php
+if (init('groupBy', 'day') == 'day') {
+	echo '<a class="btn btn-primary btn-sm" href="index.php?v=d&m=energy&p=panel&groupBy=day&energyType=' . init('energyType', 'electricity') . '&object_id=' . $object->getId() . '">{{Jour}}</a> ';
+} else {
+	echo '<a class="btn btn-default btn-sm" href="index.php?v=d&m=energy&p=panel&groupBy=day&energyType=' . init('energyType', 'electricity') . '&object_id=' . $object->getId() . '">{{Jour}}</a> ';
+}
+if (init('groupBy', 'day') == 'month') {
+	echo '<a class="btn btn-primary btn-sm" href="index.php?v=d&m=energy&p=panel&groupBy=month&energyType=' . init('energyType', 'electricity') . '&object_id=' . $object->getId() . '">{{Mois}}</a> ';
+} else {
+	echo '<a class="btn btn-default btn-sm" href="index.php?v=d&m=energy&p=panel&groupBy=month&energyType=' . init('energyType', 'electricity') . '&object_id=' . $object->getId() . '">{{Mois}}</a> ';
+}
+?>
+   <span class="pull-right">
+    <?php
+if (config::byKey('enableElectricity', 'energy') == 1) {
+	if (init('energyType', 'electricity') == 'electricity') {
+		echo '<a class="btn btn-success btn-sm" href="index.php?v=d&m=energy&p=panel&groupBy=' . init('groupBy', 'day') . 'energyType=electricity&object_id=' . $object->getId() . '"><i class="fa fa-bolt"></i> Electricité</a> ';
+	} else {
+		echo '<a class="btn btn-default btn-sm" href="index.php?v=d&m=energy&p=panel&groupBy=' . init('groupBy', 'day') . '&energyType=electricity&object_id=' . $object->getId() . '"><i class="fa fa-bolt"></i> Electricité</a> ';
+	}
+}
+if (config::byKey('enableWater', 'energy') == 1) {
+	if (init('energyType', 'electricity') == 'water') {
+		echo '<a class="btn btn-success btn-sm" href="index.php?v=d&m=energy&groupBy=' . init('groupBy', 'day') . '&p=panel&energyType=water&object_id=' . $object->getId() . '"><i class="fa fa-tint"></i> Eau</a> ';
+	} else {
+		echo '<a class="btn btn-default btn-sm" href="index.php?v=d&m=energy&groupBy=' . init('groupBy', 'day') . '&p=panel&energyType=water&object_id=' . $object->getId() . '"><i class="fa fa-tint"></i> Eau</a> ';
+	}
+}
+if (config::byKey('enableGas', 'energy') == 1) {
+	if (init('energyType', 'electricity') == 'gas') {
+		echo '<a class="btn btn-success btn-sm" href="index.php?v=d&m=energy&groupBy=' . init('groupBy', 'day') . '&p=panel&energyType=gas&object_id=' . $object->getId() . '"><i class="fa fa-fire"></i> Gaz</a> ';
+	} else {
+		echo '<a class="btn btn-default btn-sm" href="index.php?v=d&m=energy&groupBy=' . init('groupBy', 'day') . '&p=panel&energyType=gas&object_id=' . $object->getId() . '"><i class="fa fa-fire"></i> Gaz</a> ';
+	}
+}
+?>
+</span>
+</legend>
+
+<?php if (init('energyType', 'electricity') == 'electricity') {
+	?>
+
+<div class="row">
+  <div class="col-lg-6">
+   <legend><i class="icon techno-courbes3"></i>  {{Puissance (15min/1h)}} <input id="cb_powerLissage" data-href="<?php echo 'index.php?v=d&m=energy&groupBy=' . init('groupBy', 'day') . '&p=panel&energyType=' . init('energyType', 'electricity') . '&object_id=' . init('object_id', 'day')?>" type="checkbox" data-off-color="warning" <?php if (init('powerDisplay', '1h') == '1h') {echo 'checked';}
+	?> class="pull-right"/></legend>
+   <div id='div_graphPower'></div>
+ </div>
+ <div class="col-lg-6">
+   <legend><i class="fa fa-eur"></i>  {{Coût}}</legend>
+   <div id='div_graphCost'></div>
+ </div>
+</div>
+<div class="row">
+  <div class="col-lg-6">
+    <legend><i class="icon techno-courbes4"></i>  {{Consommation par objet}}</legend>
+    <div id='div_graphDetailConsumptionByObject'></div>
+  </div>
+  <div class="col-lg-6">
+    <legend><i class="icon techno-courbes2"></i>  {{Consommation par catégorie}}</legend>
+    <div id='div_graphDetailConsumptionByCategorie'></div>
+  </div>
+</div>
+<?php } else {?>
+<legend><i class="fa fa-eur"></i>  {{Coût}}</legend>
+<div id='div_graphCost'></div>
+<legend><i class="icon techno-courbes2"></i>  {{Consommation}}</legend>
+<div id='div_graphDetailConsumption'></div>
+<?php }
+?>
 </div>
 </div>
-<?php sendVarToJs('data', $panel['data']);?>
-<?php include_file('desktop', 'panel', 'js', 'pulseCounter');?>
+
+<?php include_file('desktop', 'panel', 'js', 'energy');?>
